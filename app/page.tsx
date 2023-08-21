b2ad09f5c67d1b7700250dfa11d6e21efe956bc0
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-08-21 11:01:43 -0400

    added the text and one button, not styled

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,16 @@ export default function Home() {
           <a href="google.com"  className="border-l-2 border-r-2 border-green-700" >Contact us</a>
           <a href="google.com">Gallery</a>
         </nav>
+        <h3>Visit our <b>Food Custody</b></h3>
+        <div>
+          <h1>Feels Like a Home,</h1>
+          <h1>Tastes Like a Paradise</h1>
+        </div>
+        <p>Welcome to our Halal Restaurant, where you can indulge in a wide variety of delicious and authentic halal dishes in a warm and welcoming atmosphere. At our restaurant, we believe that food is not just about nourishing the body, but also about feeding the soul. That's why we use only the finest and skilled chefs, to create mouth-watering dishes that are sure to satisfy your cravings.</p>
+
+        <div>
+          <button type="button" className=""></button>
+        </div>
       </div>
       <div className="bg-blue-300 h-screen w-1/2">
         <Image src={Food_img} alt="photo of food" />
