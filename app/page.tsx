70b48bafadef154792b22507d5f5291e4f88c3d1
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-08-21 22:27:02 -0400

    styled both buttons

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Food_img from '../public/food-unsplash.jpg'
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen flex">
+    <main className="w-screen h-screen flex dark:text-black">
       <div className=" bg-green-100 h-screen w-1/2 grid place-items-center">
         <nav >
           <a href="google.com">Home</a>
@@ -20,7 +20,8 @@ export default function Home() {
         <p>Welcome to our Halal Restaurant, where you can indulge in a wide variety of delicious and authentic halal dishes in a warm and welcoming atmosphere. At our restaurant, we believe that food is not just about nourishing the body, but also about feeding the soul. That's why we use only the finest and skilled chefs, to create mouth-watering dishes that are sure to satisfy your cravings.</p>
 
         <div>
-          <button type="button" className=""></button>
+          <button type="button" className=" px-6 py-2 bg-green-700 border-2 border-green-700 mr-2 rounded-full"> Order Now</button>
+          <button type="button" className=" px-6 py-2 border-green-700 border-2 rounded-full ml-2"> Contact Us</button>
         </div>
       </div>
       <div className="bg-blue-300 h-screen w-1/2">
