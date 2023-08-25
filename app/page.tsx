58e31563acd6ef3105a1e334562a2cd539d6f53a
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-08-25 14:10:27 -0400

    finished the styling for the main contect

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,23 @@ import Food_img from '../public/food-unsplash.jpg'
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen flex dark:text-black">
-      <div className=" bg-green-100 h-screen w-1/2 grid place-items-center">
-        <nav >
-          <a href="google.com">Home</a>
-          <a href="google.com" className="border-l-2 border-r-2 border-green-700" >About us</a>
-          <a href="google.com"  >Menu</a>
-          <a href="google.com"  className="border-l-2 border-r-2 border-green-700" >Contact us</a>
-          <a href="google.com">Gallery</a>
+    <main className="w-screen h-screen flex dark:text-black overflow-hidden text-lg aspect-[3/2]">
+      <div className=" h-screen w-1/2 pl-16 pt-12">
+        <nav className="font-light">
+          <a href="google.com" className="pr-6">Home</a>
+          <a href="google.com" className="border-l-2 border-r-2 border-green-700 px-6" >About us</a>
+          <a href="google.com" className="px-6" >Menu</a>
+          <a href="google.com" className="border-l-2 border-r-2 border-green-700 px-6" >Contact us</a>
+          <a href="google.com" className="pl-6">Gallery</a>
         </nav>
-        <h3>Visit our <b>Food Custody</b></h3>
-        <div>
+        <h3 className=" w-fit my-14 text-xl">Visit our <span className="font-bold">Food Custody</span></h3>
+        <div className="text-3xl w-fit font-bold mb-6">
           <h1>Feels Like a Home,</h1>
           <h1>Tastes Like a Paradise</h1>
         </div>
-        <p>Welcome to our Halal Restaurant, where you can indulge in a wide variety of delicious and authentic halal dishes in a warm and welcoming atmosphere. At our restaurant, we believe that food is not just about nourishing the body, but also about feeding the soul. That's why we use only the finest and skilled chefs, to create mouth-watering dishes that are sure to satisfy your cravings.</p>
+        <p className="w-2/4 text-xs font-light">Welcome to our Halal Restaurant, where you can indulge in a wide variety of delicious and authentic halal dishes in a warm and welcoming atmosphere. At our restaurant, we believe that food is not just about nourishing the body, but also about feeding the soul. That's why we use only the finest and skilled chefs, to create mouth-watering dishes that are sure to satisfy your cravings.</p>
 
-        <div>
+        <div className=" w-fit mt-6">
           <button type="button" className=" px-6 py-2 bg-green-700 border-2 border-green-700 mr-2 rounded-full"> Order Now</button>
           <button type="button" className=" px-6 py-2 border-green-700 border-2 rounded-full ml-2"> Contact Us</button>
         </div>
