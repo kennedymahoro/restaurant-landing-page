d0cdf163568b00859696bbbe9b60c0f0da61f945
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-08-29 11:11:30 -0400

    tried to fix the issue related to sizing, but no luck for now

--- app/responsive/page.tsx
+++ app/responsive/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
-import Food_img from '/public/food-unsplash.jpg'
+//import Food_img from '/public/food-unsplash.jpg'
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen flex flex-col sm:overflow-hidden text-lg bg-blue-200 sm:flex-row md:bg-red-200">
-      <div className=" h-screen w-screen grid place-items-center md:place-items-start md:pl-20 pt-3 sm:pt-12 sm:w-1/2">
+    <main className="w-full h-full flex flex-col sm:overflow-hidden text-lg bg-blue-200 sm:flex-row md:bg-red-200">
+      <div className=" h-full w-screen grid place-items-center md:place-items-start md:pl-20 pt-3 sm:pt-12 sm:w-1/2">
         <nav className="font-light invisible absolute sm:static">
           <button className="visible sm:invisible">B</button>
           <ul className="flex invisible sm:visible">
@@ -26,11 +26,11 @@ export default function Home() {
           </ul>
         </nav>
         <h3 className=" w-fit my-5 sm:my-14 text-xl">Visit our <span className="font-bold">Food Custody</span></h3>
-        <div className="text-3xl w-fit font-bold mb-6">
+        <div className="text-xl md:text-3xl w-fit font-bold mb-6">
           <h1>Feels Like a Home,</h1>
           <h1>Tastes Like a Paradise</h1>
         </div>
-        <p className="w-2/4 text-xs font-light">Welcome to our Halal Restaurant, where you can indulge in a wide variety of delicious and authentic halal dishes in a warm and welcoming atmosphere. At our restaurant, we believe that food is not just about nourishing the body, but also about feeding the soul. That's why we use only the finest and skilled chefs, to create mouth-watering dishes that are sure to satisfy your cravings.</p>
+        <p className="w-3/4 md:2/4 text-base font-light">Welcome to our Halal Restaurant, where you can indulge in a wide variety of delicious and authentic halal dishes in a warm and welcoming atmosphere. At our restaurant, we believe that food is not just about nourishing the body, but also about feeding the soul. That's why we use only the finest and skilled chefs, to create mouth-watering dishes that are sure to satisfy your cravings.</p>
 
         <div className=" w-fit mt-6">
           <button type="button" className=" px-6 py-2 bg-green-700 border-2 border-green-700 mr-2 rounded-full"> Order Now</button>
@@ -41,9 +41,10 @@ export default function Home() {
           <p className="border-t-4">Dine with Food Custody</p>
         </div>
       </div>
-      <div className="bg-blue-300 h-screen w-full md:w-1/2">
-        <Image src={Food_img} alt="photo of food" />
-      </div>
+      {//<div className="bg-blue-300 h-screen w-full md:w-1/2">
+       // <Image src={Food_img} alt="photo of food" priority="true" />
+      //</div>
+      }
     </main>
   )
 }
