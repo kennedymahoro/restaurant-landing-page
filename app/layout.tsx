9c7115b48379797b3d0e4404241d416fc2c0efae
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-08-29 11:09:05 -0400

    removed the classname from body.

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="w-fit h-fit">{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
